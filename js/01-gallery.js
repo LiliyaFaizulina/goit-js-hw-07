78754c0ca35fb5127118f618a6da22a8dd0345d1
Лілія <lily.fed85@gmail.com>
2022-08-29 08:59:17 +0300

    add script

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,62 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+let instance;
+
+galleryRef.innerHTML = createGalleryMarkup(galleryItems);
+galleryRef.addEventListener('click', openModal);
+
+function createGalleryMarkup(arr) {
+  return arr.reduce(
+    (acc, { preview, original, description }) =>
+      acc +
+      `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+    </a>
+    </div>`,
+    ''
+  );
+}
+
+function openModal(evt) {
+  evt.preventDefault();
+
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+  const currentUrl = evt.target.dataset.source;
+
+  instance = basicLightbox.create(
+    `<img src = "${currentUrl}" alt ="${evt.target.alt}" width="800" height="600" />`
+  );
+  instance.show(addModalListeners);
+}
+
+function onEscKeydown(evt) {
+  if (evt.code !== 'Escape') {
+    return;
+  }
+  instance.close(removeModalListeners);
+}
+
+function onModalClick() {
+  instance.close(removeModalListeners);
+}
+
+function addModalListeners() {
+  document.addEventListener('keydown', onEscKeydown);
+  instance.element().addEventListener('click', onModalClick);
+}
+
+function removeModalListeners() {
+  document.removeEventListener('keydown', onEscKeydown);
+  instance.element().removeEventListener('click', onModalClick);
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.innerHTML = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(arr) {
+  return arr.reduce(
+    (acc, { preview, original, description }) =>
+      acc +
+      `<a class="gallery__item" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        />
+    </a>`,
+    ''
+  );
+}
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
